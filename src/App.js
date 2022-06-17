64daf00277c8f53c0ef2fe3bf701f27b11abd407
Liqingcathy <cathy.glc@gmail.com>
2022-06-17 13:57:05 -0700

    complete func lift up and pass down finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -15,14 +16,28 @@ const TASKS = [
   },
 ];
 
+
 const App = () => {
+  const [tasks, setComplete] = useState(TASKS);
+  //update complete 
+const updatedComplete = (id) => {
+  console.log('inside update completed');
+  const changedTasks = [...tasks];
+  for (let t of changedTasks){
+    if (t.id === id){
+      t.isComplete = !t.isComplete;
+    }
+  }
+  setComplete(changedTasks);
+};
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={TASKS} updatedComplete={updatedComplete}/>}</div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, updatedComplete }) => {
+  // const [complete, setComplete] = useState(isComplete);
+  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        // onClick={() => setComplete(!complete)}
+        onClick={() => updatedComplete(id)}
       >
         {title}
       </button>
@@ -24,6 +25,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  updatedComplete: PropTypes.func.isRequired,
 };
 
 export default Task;
